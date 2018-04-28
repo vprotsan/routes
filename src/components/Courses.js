c8867878107e320ee561e8c08183b37ca1915297
Valeria Protsan <valeriyapro@gmail.com>
2018-04-27 22:46:11 -0400

    routes added

--- src/components/Courses.js
+++ src/components/Courses.js
@@ -1,17 +1,26 @@
 import React from 'react';
+import { Route } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
+
+
+import Css from './courses/CSS';
+import Html from './courses/HTML';
+import JavaScript from './courses/JavaScript';
 
 const Courses = () => (
   <div className="main-content courses">
     <div className="course-header group">
-      <h2>Courses</h2> 
+      <h2>Courses</h2>
       <ul className="course-nav">
-        <li><a href='/courses/html'>HTML</a></li>
-        <li><a href='/courses/css'>CSS</a></li>
-        <li><a href='/courses/javascript'>JavaScript</a></li>
+        <li><NavLink to='/courses/html'>HTML</NavLink></li>
+        <li><NavLink to='/courses/css'>CSS</NavLink></li>
+        <li><NavLink to='/courses/javascript'>JavaScript</NavLink></li>
       </ul>
     </div>
-    
-    {/* Write routes here... */}
+
+    <Route exact path="/courses/html" component={Html}/>
+    <Route exact path="/courses/css" component={Css}/>
+    <Route exact path="/courses/javascript" component={JavaScript}/>
   </div>
 );
 
